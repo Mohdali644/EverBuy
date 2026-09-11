e3144fd264d51901624abb4ae35cad5e4725d828
Mohdali644 <Envied94@gmail.com>
2026-09-11 22:59:39 +0530

    intial update

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -30,7 +30,7 @@ export default function Orders() {
           onClick={() => navigate('/')}
           className="translate-y-1/2 flex items-center gap-2 mb-3 text-white hover:text-white font-bold transition-all hover:-translate-x-1 bg-white/20 hover:bg-[#f78902] px-4 py-2 md:px-5 md:py-2.5 rounded-full backdrop-blur-md border border-white/30 shadow-md"
         >
-          <i className="fa-solid fa-arrow-left text-sm md:text-base"></i> <span className="hidden sm:inline">Return to Store</span>
+          <i className="fa-solid fa-arrow-left text-sm md:text-base"></i> <span className="hidden sm:inline">Return to Home</span>
         </button>
 
 
