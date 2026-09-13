5edc5c58b56af5cab21d8f61f8bfd5bc5b98116e
Mohdali644 <Envied94@gmail.com>
2026-09-13 23:03:06 +0530

    fix: modulate code for readability

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -116,7 +116,7 @@ export default function CategoryPage() {
           onClick={() => navigate('/')}
           className="fixed top-32 left-6 z-50 flex items-center gap-2 text-black hover:text-white font-bold transition-all hover:-translate-x-1 bg-white/20 hover:bg-[#f78902] px-4 py-2 md:px-5 md:py-2.5 rounded-full backdrop-blur-md border-white/20 shadow-md"
         >
-          <i className="fa-solid fa-arrow-left text-sm md:text-base"></i> <span className="hidden sm:inline">Back to Home</span>
+          <i className="fa-solid fa-arrow-left text-center md:text-base"></i> <span className="hidden text-center sm:inline">Back to Home</span>
         </button>
 
       </div>
